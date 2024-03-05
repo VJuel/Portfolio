c735fc051c19da4b8add4aef9b70b7090fb35c1e
Vicktor Juhel <98396727+VJuel@users.noreply.github.com>
2024-03-05 22:26:30 +0100

    try fix error typescript children provider

--- src/features/providers/MyCustomNextIntlClientProvider.tsx
+++ src/features/providers/MyCustomNextIntlClientProvider.tsx
@@ -1,29 +1,32 @@
-"use client"
-
-import { NextIntlClientProvider } from "next-intl"
+import { ReactNode } from "react"
+import { NextIntlProvider } from "next-intl"
 
 export default function MyCustomNextIntlClientProvider({
   locale,
   timeZone,
   now,
+  children, // Add children here
   ...props
 }: {
   locale: string
   timeZone: string
   now: Date
+  children: ReactNode // Define the type for children
   props: any
 }) {
   return (
-    <NextIntlClientProvider
+    <NextIntlProvider
       // Define non-serializable props here
       defaultTranslationValues={{
-        i: (text) => <i>{text}</i>,
+        i: (text: ReactNode) => <i>{text}</i>,
       }}
       // Make sure to forward these props to avoid markup mismatches
       locale={locale}
       timeZone={timeZone}
       now={now}
       {...props}
-    />
+    >
+      {children} {/* Pass children to the provider */}
+    </NextIntlProvider>
   )
 }
