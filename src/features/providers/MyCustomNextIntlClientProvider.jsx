00ebce0981a7833be5d312dce7451c199bdcee66
Vicktor Juhel <98396727+VJuel@users.noreply.github.com>
2024-03-06 01:09:25 +0100

    fix next-intl provider

--- src/features/providers/MyCustomNextIntlClientProvider.jsx
+++ src/features/providers/MyCustomNextIntlClientProvider.jsx
@@ -1,32 +1,20 @@
-import { ReactNode } from "react"
+"use client"
 import { NextIntlClientProvider } from "next-intl"
 
 export default function MyCustomNextIntlClientProvider({
   locale,
   timeZone,
   now,
-  children, // Add children here
   ...props
-}: {
-  locale: string
-  timeZone: string
-  now: Date
-  children: ReactNode // Define the type for children
-  props: any
 }) {
   return (
     <NextIntlClientProvider
       // Define non-serializable props here
-      defaultTranslationValues={{
-        i: (text: ReactNode) => <i>{text}</i>,
-      }}
       // Make sure to forward these props to avoid markup mismatches
       locale={locale}
       timeZone={timeZone}
       now={now}
       {...props}
-    >
-      {children} {/* Pass children to the provider */}
-    </NextIntlClientProvider>
+    />
   )
 }
